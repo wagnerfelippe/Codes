88b60157313caade0f6d81f3c292e3b669105783
Wagner Felippe <38478874+wagnerfelippe@users.noreply.github.com>
2023-12-08 17:30:05 -0300

    Update Exercícios2.py

--- Exercícios2.py
+++ Exercícios2.py
@@ -7,39 +7,130 @@
 
 #https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html - Documentação 
 
-#importando a biblioteca para visualização de gráficos
-import matplotlib.pyplot as plt
-
-x = [1,3,5,7,9]
-y = [2,3,7,1,0]
-z = [20,5,100,33,10]
-
-#criando as variáveis para legendas
-titulo = "Scatterplot: gráfico de dispersão"
-eixox = "Eixo X"
-eixoy = "Eixo Y"
-
-#Criando meu título
-plt.title(titulo)
-
-#Criando as legendas do eixo
-plt.xlabel(eixox)
-plt.ylabel(eixoy)
-
-#Gráfico de dispersão + um gráfico de linha ligando os pontos, mudando a cor dos pontos para vermelho, usando um marcador bolinha e aumentando ele para 200.
-#Além disso, mudamos a linha de sólida para traço e traço.
-#Outro ponto é que mudamos o tamanho da bola de acordo com os valores da lista z. Podemos mudar para o tamanho da lista y ou x, dependendo do que o mesmo deseja.
-plt.scatter(x,y, label = 'Meus pontos', color = 'r', marker='.', s=z)
-plt.plot(x,y, color='#009900', linestyle='--')
-plt.legend()
-
-#mostrando no gráfico como fico os valores no gráfico de linhas
-plt.show()
-
-
-
-
-
+##importando a biblioteca para visualização de gráficos
+#import matplotlib.pyplot as plt
+#
+#x = [1,3,5,7,9]
+#y = [2,3,7,1,0]
+#z = [20,5,100,33,10]
+#
+##criando as variáveis para legendas
+#titulo = "Scatterplot: gráfico de dispersão"
+#eixox = "Eixo X"
+#eixoy = "Eixo Y"
+#
+##Criando meu título
+#plt.title(titulo)
+#
+##Criando as legendas do eixo
+#plt.xlabel(eixox)
+#plt.ylabel(eixoy)
+#
+##Gráfico de dispersão + um gráfico de linha ligando os pontos, mudando a cor dos pontos para vermelho, usando um marcador bolinha e aumentando ele para 200.
+##Além disso, mudamos a linha de sólida para traço e traço.
+##Outro ponto é que mudamos o tamanho da bola de acordo com os valores da lista z. Podemos mudar para o tamanho da lista y ou x, dependendo do que o mesmo deseja.
+#plt.scatter(x,y, label = 'Meus pontos', color = 'r', marker='.', s=z)
+#plt.plot(x,y, color='#009900', linestyle='--')
+#plt.legend()
+#
+##mostrando no gráfico como fico os valores no gráfico de linhas
+#plt.show()
+#
+#
+#
+#
+#color: cor (ver exemplos abaixo)
+#
+#label: rótulo
+#
+#linestyle: estilo de linha (ver exemplos abaixo)
+#
+#linewidth: largura da linha
+#
+#marker: marcador (ver exemplos abaixo)
+#
+#
+#
+#CORES (color)
+#'b' blue
+#
+#'g' green
+#
+#'r' red
+#
+#'c' cyan
+#
+#'m' magenta
+#
+#'y' yellow
+#
+#'k' black
+#
+#'w' white
+#
+#
+#
+#Marcadores (marker)
+#'.' point marker
+#
+#',' pixel marker
+#
+#'o' circle marker
+#
+#'v' triangle_down marker
+#
+#'^' triangle_up marker
+#
+#'<' triangle_left marker
+#
+#'>' triangle_right marker
+#
+#'1' tri_down marker
+#
+#'2' tri_up marker
+#
+#'3' tri_left marker
+#
+#'4' tri_right marker
+#
+#'s' square marker
+#
+#'p' pentagon marker
+#
+#'*' star marker
+#
+#'h' hexagon1 marker
+#
+#'H' hexagon2 marker
+#
+#'+' plus marker
+#
+#'x' x marker
+#
+#'D' diamond marker
+#
+#'d' thin_diamond marker
+#
+#'|' vline marker
+#
+#'_' hline marker
+#
+#
+#
+#
+#
+#Tipos de linha (linestyle)
+#'-' solid line style
+#
+#'--' dashed line style
+#
+#'-.' dash-dot line style
+#
+#':' dotted line style
+#
+#
+#
+#Fonte: https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.html
 
 
 
